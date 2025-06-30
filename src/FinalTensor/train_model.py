d967cd4e9e0bb71a3d826f75f348143cf8e9f6c8
Raul Gomez <rgomez@tec.mx>
2025-06-30 10:27:05 -0600

    Restoring project to linux machine

--- src/FinalTensor/train_model.py
+++ src/FinalTensor/train_model.py
@@ -21,9 +21,46 @@ def main():
         "Estuvo regular, algunas partes fueron interesantes",
         "Excelente docente, aprendí mucho",
         "Muy aburrido, no volvería a tomar esta materia",
-        "No estuvo tan mal, aunque podría mejorar"
+        "No estuvo tan mal, aunque podría mejorar",
+        "El curso fué demasiado rápido y no pude seguir el ritmo",
+        "Las fechas de los quizes debieron estar mejor distribuidas",
+        "La situación problema tenía poco que ver con lo que vimos en clase",
+        "El profesor contesta todas las dudas y es muy paciente",
+        "En realidad está todo bien",
+        "Sus clases son decentes",
+        "Puede mejorar el enfoque de sus clases",
+        "Los métodos que utiliza lo hace parecer más complicado de lo que es",
+        "Demuestra gran conocimiento de los temas",
+        "No hace la clase dinámica",
+        "Clases tediosas",
+        "No brinda apoyo",
+        "Me hubiera gustado entender un poco mejor los temas del examen final",
+        "Que incentive más la participación de los alumnos",
+        "Muestra un dominio de todos los temas que se vieron en clase"
     ]
-    etiquetas = ['positivo', 'negativo', 'neutral', 'positivo', 'negativo', 'neutral']
+    etiquetas = [
+        'positivo',
+        'negativo',
+        'neutral',
+        'positivo',
+        'negativo',
+        'neutral',
+        'negativo',
+        'negativo',
+        'negativo',
+        'positivo',
+        'positivo',
+        'neutral',
+        'neutral',
+        'negativo',
+        'positivo',
+        'negativo',
+        'negativo',
+        'negativo',
+        'negativo',
+        'neutral',
+        'positivo'
+    ]
 
     label_map = {'positivo': 0, 'negativo': 1, 'neutral': 2}
     y = to_categorical([label_map[e] for e in etiquetas], num_classes=3)
